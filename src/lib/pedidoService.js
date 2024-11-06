b0e5461633401c274ca54ada841f52dfb28a24c0
Henry <henryortegon@gmail.com>
2024-11-06 07:54:06 -0500

    funcionalidad enviar pedido, bd(pedidos), funcionalidad kitchen-orders

---
+++ src/lib/pedidoService.js
@@ -0,0 +1,16 @@
+// lib/pedidoService.js insercion del nuevo pedido
+import { connectDB } from './mongodb';
+
+export async function crearPedido(pedidoData) {
+  const { db } = await connectDB();
+  const collection = db.collection('pedidos');
+  
+  const nuevoPedido = {
+    ...pedidoData,
+    fecha: new Date(),
+    estado: 'pendiente'
+  };
+
+  const resultado = await collection.insertOne(nuevoPedido);
+  return resultado.insertedId;
+}
